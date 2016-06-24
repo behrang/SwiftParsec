f0889e6c19186becf15acec1d61499ff3b49f302
Behrang Noruzi Niya <behrangn@gmail.com>
2016-06-24 17:37:19 +0430

    Improve simple csv example

--- example-csv-simple/csv3.swift
+++
@@ -1,8 +0,0 @@
-// let csvFile3 = endBy(line3, eol3)
-// let line3 = sepBy(cell3, char(","))
-// let cell3 = many(noneOf([",", "\n"])) >>- { xs -> Consumed<String, String.CharacterView> in create(String(xs)) }
-// let eol3 = char("\n")
-
-// func parseCSV3 (_ input: String) {
-//   print(csvFile3(State(input.characters)))
-// }
---
+++ example-csv-simple/main.swift
@@ -0,0 +1,33 @@
+import SwiftParsec
+
+func csv () -> Parser<[[String]], String.CharacterView>.T {
+  return endBy(line(), char("\n"))
+}
+
+func line () -> Parser<[String], String.CharacterView>.T {
+  return sepBy(cell(), char(","))
+}
+
+func cell () -> Parser<String, String.CharacterView>.T {
+  return many1(noneOf(",\n")) >>- { chars in create(String(chars)) }
+}
+
+func main () {
+  if Process.arguments.count != 2 {
+    print("Usage: \(Process.arguments[0]) csv_file")
+  } else {
+    let result = try! parse(csv(), file: Process.arguments[1])
+    switch result {
+    case let .left(err): print(err)
+    case let .right(x): format(x)
+    }
+  }
+}
+
+func format (_ data: [[String]]) {
+  data.forEach{ item in
+    print(item.joined(separator: "\t "))
+  }
+}
+
+main()
