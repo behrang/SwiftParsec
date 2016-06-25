02bda0fd5f1bd493ba07df8945f1d410d1429b85
Behrang Noruzi Niya <behrangn@gmail.com>
2016-06-25 12:38:58 +0430

    Add doc for parse from file

--- SwiftParsec/String.swift
+++ SwiftParsec/String.swift
@@ -1,3 +1,16 @@
+/**
+    `parse(p, file: filePath)` runs a string parser `p` on the
+    input read from `filePath` using 'String(contentsOfFile: filePath)'.
+    Returns either a 'ParseError' ('left') or a value of type `a` ('right').
+
+    func main () {
+      let result = try! parse(numbers(), file: "digits.txt")
+      switch result {
+      case let .left(err): print(err)
+      case let .right(xs): print(sum(xs))
+      }
+    }
+*/
 public func parse<a, c: Collection> (_ p: Parser<a, c>.T, file: String) throws -> Either<ParseError, a> {
   let contents = try String(contentsOfFile: file)
   return parse(p, file, contents.characters as! c)
