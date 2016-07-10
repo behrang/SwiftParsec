fa9e7e121fa5c4137f3d9d4658b85542902852fe
Behrang Noruzi Niya <behrangn@gmail.com>
2016-07-10 19:30:02 +0430

    Add default target to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
   targets: [
+    Target(name: "SwiftParsec"),
     Target(name: "example-csv-simple", dependencies: [.Target(name: "SwiftParsec")]),
     Target(name: "example-csv-advanced", dependencies: [.Target(name: "SwiftParsec")]),
     Target(name: "example-json", dependencies: [.Target(name: "SwiftParsec")]),
