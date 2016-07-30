69defae8bbc7d35159fe1e7ef10fc21237e7de89
Behrang Noruzi Niya <behrangn@gmail.com>
2016-07-30 09:30:54 +0430

    Replace NSCharacterSet with CharacterSet

--- SwiftParsec/Compatibility.swift
+++ SwiftParsec/Compatibility.swift
@@ -1,43 +1,41 @@
-// TODO: Consider using CharacterSet instead of NSCharacterSet
-// https://developer.apple.com/reference/foundation/characterset
 import Foundation
 
 func isSpace (_ c: Character) -> Bool {
-  let whitespaces = NSCharacterSet.whitespacesAndNewlines()
+  let whitespaces = CharacterSet.whitespacesAndNewlines
   return String(c).rangeOfCharacter(from: whitespaces) != nil
 }
 
 func isUpper (_ c: Character) -> Bool {
-  let uppers = NSCharacterSet.uppercaseLetters()
+  let uppers = CharacterSet.uppercaseLetters
   return String(c).rangeOfCharacter(from: uppers) != nil
 }
 
 func isLower (_ c: Character) -> Bool {
-  let lowers = NSCharacterSet.lowercaseLetters()
+  let lowers = CharacterSet.lowercaseLetters
   return String(c).rangeOfCharacter(from: lowers) != nil
 }
 
 func isAlphaNum (_ c: Character) -> Bool {
-  let alphaNums = NSCharacterSet.alphanumerics()
+  let alphaNums = CharacterSet.alphanumerics
   return String(c).rangeOfCharacter(from: alphaNums) != nil
 }
 
 func isLetter (_ c: Character) -> Bool {
-  let letters = NSCharacterSet.letters()
+  let letters = CharacterSet.letters
   return String(c).rangeOfCharacter(from: letters) != nil
 }
 
 func isDigit (_ c: Character) -> Bool {
-  let digits = NSCharacterSet.decimalDigits()
+  let digits = CharacterSet.decimalDigits
   return String(c).rangeOfCharacter(from: digits) != nil
 }
 
 func isHexDigit (_ c: Character) -> Bool {
-  let hexDigits = NSCharacterSet(charactersIn: "0123456789aAbBcCdDeEfF")
+  let hexDigits = CharacterSet(charactersIn: "0123456789aAbBcCdDeEfF")
   return String(c).rangeOfCharacter(from: hexDigits) != nil
 }
 
 func isOctDigit (_ c: Character) -> Bool {
-  let octDigits = NSCharacterSet(charactersIn: "01234567")
+  let octDigits = CharacterSet(charactersIn: "01234567")
   return String(c).rangeOfCharacter(from: octDigits) != nil
 }
